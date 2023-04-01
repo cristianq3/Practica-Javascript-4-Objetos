da175b9fac3ceb2cc016f662972b98af813993c9
cristianq3 <cristianq3@gmail.com>
2023-04-01 19:01:12 -0300

    Se crean las clases y prototipos

--- js/Ejercicio-7.js
+++ js/Ejercicio-7.js
@@ -1,9 +1,50 @@
-class Contacto(nombre, telefono) {
-    #nombre;
-    #telefono;
-    constructor(nombre, telefono) {
-        this.#nombre = nombre;
-        this.#telefono = telefono;
-    }
-  
+class Contacto {
+  #nombre;
+  #telefono;
+  constructor(nombre, telefono) {
+    this.#nombre = nombre;
+    this.#telefono = telefono;
+  }
+
+  get nombre() {
+    return this.#nombre;
+  }
+
+  set nombre(nuevoNombre) {
+    this.#nombre = nuevoNombre;
+  }
+
+  get telefono() {
+    return this.#telefono;
+  }
+
+  set nombre(nuevoTelefono) {
+    this.#nombre = nuevoTelefono;
+  }
 }
+
+class Agenda {
+  #contactos;
+  #tamanio;
+  constructor(tamanio = 10) {
+    this.#contactos = [];
+    this.#tamanio = tamanio;
+  }
+
+  get contactos() {
+    return this.#contactos;
+  }
+
+  set contactos(nuevoContacto) {
+    this.#contactos.push(nuevoContacto);
+  }
+
+  get tamanio() {
+    return this.#tamanio;
+  }
+
+  set tamanio(nuevoTelefono) {
+    this.#tamanio = nuevoTamanio;
+  }
+}
+ 
